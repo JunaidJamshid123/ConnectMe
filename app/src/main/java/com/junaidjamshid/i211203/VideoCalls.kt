868f3e1842538bf7a9e207ba1b09d7c7f9eebbe3
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2025-02-12 21:35:11 +0500

    call page Developed

--- app/src/main/java/com/junaidjamshid/i211203/VideoCalls.kt
+++ app/src/main/java/com/junaidjamshid/i211203/VideoCalls.kt
@@ -3,10 +3,9 @@ package com.junaidjamshid.i211203
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Calls : AppCompatActivity() {
+class VideoCalls : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
-        setContentView(R.layout.activity_calls)
+        setContentView(R.layout.activity_video_calls)
     }
 }
