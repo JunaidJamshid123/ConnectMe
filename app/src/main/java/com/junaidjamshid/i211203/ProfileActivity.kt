ee19c2eab52342ba19be5e256aabd5d46d69e708
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2025-02-15 23:45:59 +0500

    Profile Page Developed

--- app/src/main/java/com/junaidjamshid/i211203/ProfileActivity.kt
+++ app/src/main/java/com/junaidjamshid/i211203/ProfileActivity.kt
@@ -2,10 +2,26 @@ package com.junaidjamshid.i211203
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_profile)
+
+        val gridView = findViewById<GridView>(R.id.grid_view)
+
+        val imageList = listOf(
+            R.drawable.junaid1,
+            R.drawable.junaid2,
+            R.drawable.junaid1,
+            R.drawable.junaid2,
+            R.drawable.junaid1
+        )
+
+        val adapter = ImageAdapter(this, imageList)
+        gridView.adapter = adapter
+
     }
 }
