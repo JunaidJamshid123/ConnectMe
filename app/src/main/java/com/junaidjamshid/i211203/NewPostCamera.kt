40bbb7fafdf4b36c8c9ff7a1d8d88df21367dd5b
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2025-02-16 14:02:52 +0500

    New Post Next Page Developed

--- app/src/main/java/com/junaidjamshid/i211203/NewPostCamera.kt
+++ app/src/main/java/com/junaidjamshid/i211203/NewPostCamera.kt
@@ -2,10 +2,14 @@ package com.junaidjamshid.i211203
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class NewPostCamera : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide();
         setContentView(R.layout.activity_new_post_camera)
+
     }
 }
