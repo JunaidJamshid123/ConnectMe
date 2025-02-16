f5250767aa9acf2807a4ce6506091ba90547b19d
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2025-02-16 21:47:07 +0500

    Finalizing....

--- app/src/main/java/com/junaidjamshid/i211203/chats.kt
+++ app/src/main/java/com/junaidjamshid/i211203/chats.kt
@@ -1,12 +1,26 @@
 package com.junaidjamshid.i211203
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class chats : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide() // Hide Action Bar
         setContentView(R.layout.activity_chats)
+        val telephone = findViewById<ImageView>(R.id.telephone);
+        val camera = findViewById<ImageView>(R.id.camera);
+
+        telephone.setOnClickListener{
+            val intent = Intent(this,Calls::class.java);
+            startActivity(intent);
+        }
+
+        camera.setOnClickListener{
+            val intent = Intent(this,VideoCalls::class.java);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/junaidjamshid/i211203/splashScreen.kt
+++ app/src/main/java/com/junaidjamshid/i211203/splashScreen.kt
@@ -1,11 +1,20 @@
 package com.junaidjamshid.i211203
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class splashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginScreem::class.java))
+            finish()
+        }, 3000) // 3-second delay
     }
 }
+
