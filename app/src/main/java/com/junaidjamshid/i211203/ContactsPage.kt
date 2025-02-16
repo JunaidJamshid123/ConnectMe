94efb5948f6ed9f89b3d5f9d298a585bbaa53f85
JunaidJamshid123 <jamshidjunaid763@gmail.com>
2025-02-16 15:19:40 +0500

    New Post Camera Page Developed

---
+++ app/src/main/java/com/junaidjamshid/i211203/ContactsPage.kt
@@ -0,0 +1,11 @@
+package com.junaidjamshid.i211203
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts_page)
+    }
+}
